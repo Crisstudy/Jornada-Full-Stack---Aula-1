b5caed128db62921a82421dce87364dd5e27ce22
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-05-07 09:36:44 -0300

    first commit

---
+++ Gabarito da Aula 1/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+console.log("Hello, world!");
+
+  return <h1>Hello, world!</h1>;
+}
+
+export default App
---
+++ Gabarito da Aula 1/src/App2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2
+
+
+// Arrow function
+// const App2 = () => <h1>Hello, world!</h1>; 
